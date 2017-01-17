9bc6295c255c44843cc698ff581446ea23cd78d6
hoff <micklinghoff@gmail.com>
2017-01-18 01:01:09 +0800

    add slugify lib

--- backend/author.py
+++ backend/author.py
@@ -1,12 +1,32 @@
 from google.appengine.ext import ndb
 from shared import RequestHandler, return_json
+from slugify import slugify
 
 """
 The Author Model
 """
 class Author(ndb.Model):
+    """
+    the id is set when an entitiy is created, and is the slugified version
+    of the name.
+    """
     name = ndb.StringProperty()
+    email = ndb.StringProperty()
 
+    @classmethod
+    def create(cls, name, email, about = None):
+        """
+        creates an auther with a given name, email, and (optional) about.
+        returns the newly created author's key.
+        """
+        author = cls(
+            id = slugify(name),
+            name = name,
+            email = email,
+            about = about
+        ).put()
+        return author
+        
 
 """
 The Author Endpoint
